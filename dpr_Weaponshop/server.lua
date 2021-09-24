e7f8dcc0e9358cd925c8ce20f67b3265ad6add46
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-24 20:23:52 +0200

    Update server.lua

--- dpr_Weaponshop/server.lua
+++ dpr_Weaponshop/server.lua
@@ -9,32 +9,25 @@ end)
 
 RegisterServerEvent('dpr_Weaponshop:AchatArme')
 AddEventHandler('dpr_Weaponshop:AchatArme', function(Label, Weapon, Price)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
+	local _src = source
+	local xPlayer = ESX.GetPlayerFromId(_src)
 	
 	if(xPlayer.getBank() >= Price) then
 		xPlayer.removeAccountMoney('bank', Price)
 		
 		if not xPlayer.hasWeapon(Weapon) then
 			xPlayer.addWeapon(Weapon, 1)
-			local xPlayers	= ESX.GetPlayers()
-			for i=1, #xPlayers, 1 do
-				local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-				Citizen.Wait(500) 
-				TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Ammunation', 'Armurier', 'Merci pour ton achat !', 'CHAR_AMMUNATION', 1)
-				Citizen.Wait(10000)
-				TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', "Un prélèvement de ~r~"..Price.." ~s~a été effectué sur votre compte pour ~o~"..Label.." ~s~!", 'CHAR_BANK_MAZE', 1)
+			Citizen.Wait(500) 
+			TriggerClientEvent('esx:showAdvancedNotification', _src, 'Ammunation', 'Armurier', 'Merci pour ton achat !', 'CHAR_AMMUNATION', 1)
+			Citizen.Wait(10000)
+			TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', "Un prélèvement de ~r~"..Price.." ~s~a été effectué sur votre compte pour ~o~"..Label.." ~s~!", 'CHAR_BANK_MAZE', 1)
 			end
 		else
-			local xPlayers	= ESX.GetPlayers()
-			for i=1, #xPlayers, 1 do
-				local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-				Citizen.Wait(500)
-				TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Ammunation', 'Armurier', 'Tu en as déjà un sur toi !', 'CHAR_AMMUNATION', 1)
-			end
+			Citizen.Wait(500)
+			TriggerClientEvent('esx:showAdvancedNotification', _src, 'Ammunation', 'Armurier', 'Tu en as déjà un sur toi !', 'CHAR_AMMUNATION', 1)
 		end
 	else
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', "Vous n'avez pas suffisament d'argent sur votre compte bancaire ~s~!", 'CHAR_BANK_MAZE', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', "Vous n'avez pas suffisament d'argent sur votre compte bancaire ~s~!", 'CHAR_BANK_MAZE', 1)
 	end	
 end)
 
@@ -42,22 +35,18 @@ end)
 -- Accessoires --
 RegisterServerEvent('dpr_Weaponshop:AchatItem')
 AddEventHandler('dpr_Weaponshop:AchatItem', function(Label, Item, Price)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
+	local _src = source
+	local xPlayer = ESX.GetPlayerFromId(_src)
 	
 	if(xPlayer.getBank() >= Price) then
 		xPlayer.removeAccountMoney('bank', Price)
 		
 		xPlayer.addInventoryItem(Item, 1)				
-		local xPlayers	= ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			Citizen.Wait(500) 
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Ammunation', 'Armurier', 'Merci pour ton achat !', 'CHAR_AMMUNATION', 1)
-			Citizen.Wait(10000)
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', 'Un prélèvement de ~r~100$ ~s~a été effectué sur votre compte pour une lampe !', 'CHAR_BANK_MAZE', 1)
-		end
+		Citizen.Wait(500) 
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Ammunation', 'Armurier', 'Merci pour ton achat !', 'CHAR_AMMUNATION', 1)
+		Citizen.Wait(10000)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', 'Un prélèvement de ~r~100$ ~s~a été effectué sur votre compte pour une lampe !', 'CHAR_BANK_MAZE', 1)
 	else
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', "Vous n'avez pas suffisament d'argent sur votre compte bancaire ~s~!", 'CHAR_BANK_MAZE', 1)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', "Vous n'avez pas suffisament d'argent sur votre compte bancaire ~s~!", 'CHAR_BANK_MAZE', 1)
 	end	
 end)
